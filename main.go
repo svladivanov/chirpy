d68d4345d691c5d2831e394a504cb162116ba472
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-10-30 17:09:51 +0100

    handle index.html

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := &http.Server{
 		Handler: mux,
 		Addr:    ":" + port,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
