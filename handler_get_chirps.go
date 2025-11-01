dc227d2c1eb1ff9b38d2bbaaf575f19900e16f39
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-11-01 20:26:25 +0100

    get chirp by ID

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.db.GetChirps(r.Context())
@@ -21,4 +25,30 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, chirps)
+}
+
+func (cfg *apiConfig) handlerGetChirpByID(w http.ResponseWriter, r *http.Request) {
+	chirpID := r.PathValue("chirpID")
+
+	newID, err := uuid.Parse(chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "invalid chirp ID", err)
+		return
+	}
+
+	dbChirp, err := cfg.db.GetChirpByID(r.Context(), newID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "couldn't get chirp", err)
+		return
+	}
+
+	apiChirp := Chirp{
+		ID:        dbChirp.ID,
+		CreatedAt: dbChirp.CreatedAt,
+		UpdatedAt: dbChirp.UpdatedAt,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
+	}
+
+	respondWithJSON(w, http.StatusOK, apiChirp)
 }
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -14,3 +14,8 @@ RETURNING *;
 SELECT * FROM chirps
 ORDER BY created_at ASC;
 --
+
+-- name: GetChirpByID :one
+SELECT * FROM chirps
+WHERE id = $1;
+--
